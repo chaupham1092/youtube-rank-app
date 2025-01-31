de6f1e1b921b3fb21a588b7be51968afc453e553
Francis Balah <chaupham1092@gmail.com>
2025-01-31 01:07:17 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.post('/check-rank', async (req, res) => {
     const { videos, keywords } = req.body;
